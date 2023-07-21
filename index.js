43a66df873ff4aa9ab809fb072abd9826c63d985
Ole <olakas99@wp.pl>
2023-07-21 15:59:14 +0200

    start project

---
+++ index.js
@@ -0,0 +1,29 @@
+// TODO:
+function openImageFromFile()
+{
+
+}
+
+// TODO:
+function applyGrid()
+{
+
+}
+
+// TODO:
+function changeGridSize()
+{
+
+}
+
+// TODO:
+function changeGridColor()
+{
+
+}
+
+// TODO:
+function changeGridThickness()
+{
+    
+}
